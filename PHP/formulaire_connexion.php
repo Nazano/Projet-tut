302f027eec395be1e247475bef05ea1299aaed55
Bisonpasfute <thibault.cothenet@orange.fr>
2018-01-03 14:27:39 -0500

    Ajout de la création d'un compte

--- PHP/formulaire_connexion.php
+++ PHP/formulaire_connexion.php
@@ -2,26 +2,27 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Connexion</title>
+    <title>Se connecter</title>
   </head>
   <body>
     <span style="color:red">
     <?php
       session_start();
-      if(isset($_SESSION["checkConnexion"]) AND $_SESSION["checkConnexion"] == false) {
+      if($_SESSION["checkFormulaire"] == false) {
+        echo "Veuillez compléter le formulaire..";
+        $_SESSION["checkFormulaire"] = true;
+      }
+      else if(isset($_SESSION["checkConnexion"]) AND $_SESSION["checkConnexion"] == false) {
         echo "Identifiant ou mot de passe incorrect.";
         $_SESSION["checkConnexion"] = true;
-      }
-      if(isset($_SESSION["checkFormulaire"]) AND $_SESSION["checkFormulaire"] == false) {
-        echo "Veuillez compléter le formulaire entièrement pour accéder à votre compte";
-        $_SESSION["checkFormulaire"] = true;
       }
     ?>
     </span>
     <form action="connexion.php" method="post">
       Identifiant : <input type="text" name="login"> </br>
       Mot de passe : <input type="password" name="pwd"> </br>
-      <input type="submit" name="connect" value="Se connecter">
+      <input type="submit" name="connect" value="Se connecter" formaction="connexion.php">
+      <input type="submit" name="create_account" value="Créer un compte" formaction="formulaire_compte.php">
     </form>
   </body>
 </html>
