f585ac9b12e15ad07155e478ae49c10931e4496e
Antoine Julien <ajulien@MacBook-Pro-de-Antoine.local>
2018-01-07 22:23:02 -0500

    Remise en ordre

---
+++ Controleur/affichage_personnes.php
@@ -0,0 +1,16 @@
+<!--
+Fonctions qui affichent les parents et les enfants
+ -->
+
+<?php
+
+  public function afficher_parents()
+  {
+    #...
+  }
+
+  public function afficher_enfants()
+  {
+    #...
+  }
+ ?>
