0d9e87f262224913047213a1351a97f072e3417f
BurnSaibot <35008983+BurnSaibot@users.noreply.github.com>
2018-01-10 22:33:00 +0100

    updt Connexion/Inscription

--- Controleur/connexion.php
+++ Controleur/connexion.php
@@ -1,17 +1,16 @@
 <?php
      require_once("../Modele/user.php");
+     require_once("../Modele/clean.php");
      if (isset($_POST['pseudo']) && isset($_POST['pass'])) {
-                $pseudo = $_POST['pseudo'];
+                $pseudo = clean_input($_POST['pseudo']);
                 //echo "ok1";
-                $pwd = $_POST['pass'];
+                $pwd = clean_input($_POST['pass']);
                 //echo "ok2";
                 $co = (new bd("projet_tut"))->connexion();
                 //echo "ok3";
-                var_dump($pseudo);
-                var_dump($pwd);
                 $result = mysqli_query($co,"SELECT * FROM parent WHERE id = '$pseudo' AND psswd = '$pwd'") or die ("On trouve pas le membre");
                 $nbLigne= mysqli_num_rows($result);
-                echo "pseudo : " . $pseudo . "mdp : " . $pwd ."nbLigne : " . $nbLigne;
+                #echo "pseudo : " . $pseudo . "mdp : " . $pwd ."nbLigne : " . $nbLigne;
                 
                 var_dump($nbLigne);
                 //echo "ok4";
--- Controleur/inscription.php
+++ Controleur/inscription.php
@@ -1,18 +1,19 @@
 <?php
     require_once("../Modele/user.php");
+    require_once("../Modele/clean.php");
     if (isset($_POST['pseudo']) && isset($_POST['pass']) && isset($_POST['email']) && isset($_POST['nom']) && isset($_POST['prenom']) && isset($_POST['tel']) && isset($_POST['check_pass']) && isset($_POST['check_email'])) {
-        $pwd = $_POST['pass'];
-        $mail = $_POST['email'];
-        $check_pwd = $_POST['check_pass'];
-        $check_mail = $_POST['check_email'];
+        $pwd = clean_input($_POST['pass']);
+        $mail = clean_input($_POST['email']);
+        $check_pwd = clean_input($_POST['check_pass']);
+        $check_mail = clean_input($_POST['check_email']);
         
         if ( !($pwd == $check_pwd && $mail == $check_mail) ) {
             header('Location: ../Controleur/cookieMailMdp.php');
         } else {
-            $pseudo = $_POST['pseudo'];
-            $nom = $_POST['nom'];
-            $prenom = $_POST['prenom'];
-            $tel = $_POST['tel'];
+            $pseudo = clean_input($_POST['pseudo']);
+            $nom = clean_input($_POST['nom']);
+            $prenom = clean_input($_POST['prenom']);
+            $tel = clean_input($_POST['tel']);
             echo "<br/> Création du membre <br/>";
             $info = new user((new bd("projet_tut"))->connexion(),$nom,$prenom,$pseudo,$pwd,$mail,$tel);
             $info->connexion();
