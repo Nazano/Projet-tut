bf118d6cff8350c460df4e2030b6087b4c8974d8
BurnSaibot <35008983+BurnSaibot@users.noreply.github.com>
2018-01-10 21:22:59 +0100

    maj-connexion/inscription
    maj-connexion/inscription

--- Vue/formulaire_connexion.php
+++ Vue/formulaire_connexion.php
@@ -1,21 +1,25 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>Se connecter</title>
-  </head>
-  <body>
-      <span style="color:red">
-    <?php
-      require_once("../Controleur/connexion_functions.php");
-      connexion();
-    ?>
-      </span>
-    <form action="../Controleur/connexion.php" method="post">
-      Identifiant : <input type="text" name="login" value = <?php get_username(); ?> > </br>
-      Mot de passe : <input type="password" name="pwd"> </br>
-      <input type="submit" name="connect" value="Se connecter" formaction="../Controleur/connexion.php">
-      <input type="submit" name="create_account" value="Créer un compte" formaction="formulaire_compte.php">
-    </form>
-  </body>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>Connexion</title>
+    </head>	
+
+    <body>
+        <form action='../controleur/connexion.php' method='POST'>
+            <p>
+                <label for="pseudo">Pseudo : </label>
+                <input type="text" name="pseudo" value="" required/>
+            </p>
+
+            <p>
+                <label for="pass">Mot de passe : </label>
+                <input type="password" name="pass" value="" required/>
+            </p>
+
+            <input type='submit' value='Se connecter' />
+        </form>
+        
+
+    </body>
 </html>
+
